f8b87e59259424b6a04569f1f13bbdc86646907f
shouryabrightland <shourya.brightland@gmail.com>
2025-10-07 14:10:14 +0530

    divya...

---
+++ site 2.0/custum/1/q.js
@@ -0,0 +1,102 @@
+const data = `
+
+[0][Question]
+Do u like my dance?
+[options]
+no
+a lot[c]
+a little
+idk.
+[correct]
+Thank u.
+[wrong]
+Fuck off.
+
+
+[1][Question]
+Do u like my dance?
+[options]
+no
+a lot[c]
+a little
+idk.
+[correct]
+Thank u.
+[wrong]
+Fuck off.
+
+[2][Question]
+Do u like my dance?
+[options]
+aag laga di[c]
+no
+lol
+bhkkkk
+[correct]
+Thank u.
+[wrong]
+Fuck off.
+
+[3][Question]
+Do u like my dance?
+[options]
+are kyo karu me tha tha
+no tha tha at all!!
+bhkkkk tha tha ker ke
+lag ja gale se tha ker ke[c]
+[correct]
+Thank u.
+[wrong]
+Fuck off.
+
+[4][Question]
+Do u like my dance?
+[options]
+are bahut aacha[c]
+bacha tu nahi hai aacha
+chal saath nachi nachi kerte hai
+bhkkkk nachi nachi ki
+[correct]
+Thank u. nachi nachi...
+[wrong]
+Fuck off. ab no nachi nachi...
+
+[5][Question]
+Do u like my dance?
+[options]
+i hate sigma
+bhkkk teri maaa ka sigma toru
+i like sigma boys[c]
+you so naaati...
+[correct]
+Thank u. nachi nachi...
+[wrong]
+Fuck off. ab no nachi nachi...
+
+`
+
+
+
+const dec = `
+[0] Dancing Angela 
+i like to dance, thumak thumak thumak chale tu bahar ka thuma...
+
+[1] Denchak puja
+Mu kalaaaeaa mu ka balaaaeeaa, lela.... oho lela......
+
+[2] Summer lady
+haaye garmi.....
+
+[3] Anime girl
+aaaja lag ja gale se, uu tha ker ke thak thak...
+
+[4] nura fatai
+me hu nura, kamar se bhokti hu shura....
+
+[5] sigma boy
+yo... yo... aaj me uper aaasma niche, aaj me aage jamana hai pecheeee!! sigma sigma boy sigma boy sigma boy...
+`
+
+const intro = `
+introduction... likhna bul geya...
+`
